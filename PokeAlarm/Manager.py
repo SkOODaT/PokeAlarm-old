fbe4c7e4d1a3919f6200363905bdb9305439bb43
kvangent <kurtis.vangent@coyotes.usd.edu>
2017-10-22 09:43:52 -0700

    Readded team_name and team_id to raid dts.

--- PokeAlarm/Manager.py
+++ PokeAlarm/Manager.py
@@ -792,6 +792,14 @@ class Manager(object):
         guard_pokemon_id = gym['guard_pkmn_id']
         from_team_id = self.__gym_hist.get(gym_id)
 
+        if from_team_id != to_team_id:
+            # Update gym's last known team
+            self.__gym_hist[gym_id] = to_team_id
+
+        if self.__gym_settings['enabled'] is False:
+            log.debug("Gym ignored: notifications are disabled.")
+            return
+
         # Doesn't look like anything to me
         if to_team_id == from_team_id:
             log.debug("Gym ignored: no change detected")
@@ -916,6 +924,14 @@ class Manager(object):
         to_team_id = gym_info['new_team_id']
         guard_pkmn_id = gym_info['guard_pkmn_id']
         from_team_id = self.__gym_hist.get(gym_id)
+
+        if from_team_id != to_team_id:
+            # Update gym's last known team
+            self.__gym_hist[gym_id] = to_team_id
+
+        if self.__gym_settings['enabled'] is False:
+            log.debug("Gym ignored: notifications are disabled.")
+            return
 
         if gym_info['is_in_battle'] == "True":
             log.info("%s Gym under attack!", gym_info['is_in_battle'])
@@ -1213,6 +1229,9 @@ class Manager(object):
         time_str = get_time_as_str(raid['raid_end'], self.__timezone)
         start_time_str = get_time_as_str(raid['raid_begin'], self.__timezone)
 
+        # team id saved in self.__gym_hist when processing gym
+        team_id = self.__gym_hist.get(gym_id, '?')
+
         gym_info = self.__gym_info.get(gym_id, {})
 
         raid.update({
@@ -1231,7 +1250,8 @@ class Manager(object):
             'quick_move': self.__locale.get_move_name(quick_id),
             'charge_move': self.__locale.get_move_name(charge_id),
             'form': self.__locale.get_form_name(pkmn_id, raid_pkmn['form_id']),
-            'team_id': self.__locale.get_team_name(team_id)
+            'team_id': team_id,
+            'team_name': self.__locale.get_team_name(team_id)
         })
 
         threads = []
