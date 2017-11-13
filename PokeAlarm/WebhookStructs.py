88c676ff1657a25dd8c7f371b3208adfcb60db3b
sLoPPydrive <sloppydrive@gmail.com>
2017-11-13 14:37:26 +0100

    Get current team ID from raid and egg webhook data if provided
    This eliminates the need for a previous gym webhook

--- PokeAlarm/WebhookStructs.py
+++ PokeAlarm/WebhookStructs.py
@@ -201,9 +201,14 @@ class RocketMap:
         else:
             id_ = data.get('gym_id')  # RM sends the gym id
 
+        team_id = data.get('team_id', data.get('team'))
+        if team_id is not None:
+            team_id = int(team_id)
+
         egg = {
             'type': 'egg',
             'id': id_,
+            'team_id': team_id,
             'raid_level': check_for_none(int, data.get('level'), 0),
             'raid_end': raid_end,
             'raid_begin': raid_begin,
@@ -245,9 +250,14 @@ class RocketMap:
         else:
             id_ = data.get('gym_id')  # RM sends the gym id
 
+        team_id = data.get('team_id', data.get('team'))
+        if team_id is not None:
+            team_id = int(team_id)
+
         raid = {
             'type': 'raid',
             'id': id_,
+            'team_id': team_id,
             'pkmn_id': check_for_none(int, data.get('pokemon_id'), 0),
             'cp': check_for_none(int, data.get('cp'), '?'),
             'quick_id': quick_id,
